9f63a086cefa4a3e507e48bbd225a18254e3a62c
usmanghani2023 <usman.efulife@gmail.com>
2023-07-05 12:56:46 -0700

    Ans9

---
+++ Q9.dart
@@ -0,0 +1,8 @@
+void main() {
+  int number = 5;
+  int cubRoot;
+  for (var i = 1; i <= number; i++) {
+    cubRoot = i * i * i;
+    print("Number is : $i and cube of the $i is :$cubRoot");
+  }
+}
